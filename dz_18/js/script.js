4d963378640410774f65c03eb18e5b6b4039f0dd
Kvlad1199 <66115078+Kvlad1199@users.noreply.github.com>
2023-04-12 20:19:57 +0300

    Delete dz_18 directory

--- dz_18/js/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("click", function (e) {
-  const targetElement = e.target;
-
-  if (targetElement.closest(".header__burger-icon")) {
-    document.documentElement.classList.toggle("header__burger-open");
-    e.preventDefault();
-  }
-});
